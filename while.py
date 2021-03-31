0c91d01bd4c6e733515275644d17667c9b4051ef
Adam_Codes <76964302+Adam0290@users.noreply.github.com>
2021-03-31 18:19:53 +0100

    Add files via upload

---
+++ while.py
@@ -0,0 +1,93 @@
+#while loop
+
+# num = 0
+
+# while num < 10:
+#     num += 1
+#     print (num)
+
+# import random
+# rand_num = random.randint(0,50)
+# my_num = 50
+
+# while rand_num != my_num: #The not-equal-to operator ( != ) 
+#     print(rand_num)
+#     rand_num = random.randint(0,50)
+
+# print("You've found{}!".format(my_num))
+
+
+
+import random
+card_pack = [
+    "Ace Of Spades",
+    "2 Of Spades",
+    "3 Of Spades",
+    "4 Of Spades",
+    "5 Of Spades",
+    "6 Of Spades",
+    "7 Of Spades",
+    "8 Of Spades",
+    "9 Of Spades",
+    "10 Of Spades",
+    "Jack Of Spades",
+    "Queen Of Spades",
+    "King Of Spades",
+    "Ace Of Hearts",
+    "2 Of Hearts",
+    "3 Of Hearts",
+    "4 Of Hearts",
+    "5 Of Hearts",
+    "6 Of Hearts",
+    "7 Of Hearts",
+    "8 Of Hearts",
+    "9 Of Hearts",
+    "10 Of Hearts",
+    "Jack Of Hearts",
+    "Queen Of Hearts",
+    "King Of Hearts",
+    "Ace Of Clubs",
+    "2 Of Clubs",
+    "3 Of Clubs",
+    "4 Of Clubs",
+    "5 Of Clubs",
+    "6 Of Clubs",
+    "7 Of Clubs",
+    "8 Of Clubs",
+    "9 Of Clubs",
+    "10 Of Clubs",
+    "Jack Of Clubs",
+    "Queen Of Clubs",
+    "King Of Clubs",
+    "Ace Of Diamonds",
+    "2 Of Diamonds",
+    "3 Of Diamonds",
+    "4 Of Diamonds",
+    "5 Of Diamonds",
+    "6 Of Diamonds",
+    "7 Of Diamonds",
+    "8 Of Diamonds",
+    "9 Of Diamonds",
+    "10 Of Diamonds",
+    "Jack Of Diamonds",
+    "Queen Of Diamonds",
+    "King Of Diamonds",
+    "Joker"
+]
+rand_card = random.choice(card_pack)
+my_card = "King Of Spades"
+while rand_card != my_card:
+    print(rand_card)
+    rand_card = random.choice(card_pack)
+print("you've found the {}".format(my_card))
+
+# links of lists
+
+# card_deck = [
+#     ["Ace", "Two", "Three", "Four", ],
+#     ["King of Spades", "Two Hearts", "Joker", ],
+#     ["Ace", "Two", "Three", ],
+#     ["Ace", "Two", "Three", ],
+# ]
+# print(card_deck[1][2])
+
